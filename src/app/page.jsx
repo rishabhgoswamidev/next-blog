ccf55ee002f54ea164bf36fb819560d3f8e7a7ce
rishabh goswami <112328706+rishabhgoswamidev@users.noreply.github.com>
2025-10-20 16:51:14 +0530

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,19 +1,23 @@
-import Link from "next/link";
+"use client";
+
+import { Suspense } from "react";
 import styles from "./homepage.module.css";
 import Featured from "@/components/Featured/Featured";
 import CategoryList from "@/components/CategoryList/CategoryList";
 import CardList from "@/components/cardList/CardList";
 import Menu from "@/components/Menu/Menu";
 
-export default function Home(searchParams) {
+export default function Home({ searchParams }) {
+  const page = parseInt(searchParams?.page) || 1;
 
-  const page = parseInt(searchParams.page) || 1;
   return (
     <div className={styles.container}>
       <Featured />
       <CategoryList />
       <div className={styles.content}>
-        <CardList page={page}/>
+        <Suspense fallback={<div>Loading...</div>}>
+          <CardList page={page} />
+        </Suspense>
         <Menu />
       </div>
     </div>
